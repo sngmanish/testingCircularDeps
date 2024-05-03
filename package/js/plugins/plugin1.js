4c0cf418dd90cd804f2962337bbf195191e05609
Manish Singh <manish.singh@brevo.com>
2024-05-03 23:01:59 +0530

    init

---
+++ package/js/plugins/plugin1.js
@@ -0,0 +1,23 @@
+(function (global, factory) {
+  typeof exports === "object" && typeof module !== "undefined"
+    ? factory(require("editor"))
+    : typeof define === "function" && define.amd
+    ? define(["@test/editor"], factory)
+    : factory(global.Editor);
+})(this, function (E) {
+  console.log({ E });
+  ("use strict");
+  E.plugin1 = function () {
+    function f1() {
+      console.log("Hello from plugin1 f1");
+    }
+    function f2() {
+      console.log("Hello from plugin1 f2");
+    }
+
+    return {
+      f1: f1,
+      f2: f2,
+    };
+  };
+});
---
+++ test/index.js
@@ -0,0 +1,4 @@
+// import Editor from '@test/editor';
+const Editor = require('editor');
+require('editor/js/plugins/plugin1');
+console.log(Editor)
